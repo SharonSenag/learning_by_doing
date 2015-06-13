ffa6eebba30828ac7ae9ddb03bee7b76eaf7da6b
Sharon Sena <sharon.sena.g@gmail.com>
2015-06-12 21:38:45 -0600

    Passed number_of_seconds.rb in homework tester
    [#96335278]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -16,7 +16,7 @@ puts "There are #{days_in_a_week} days in a week"
 
 puts "That means there are:"
 puts "  #{seconds_in_an_hour} seconds in an hour,"
-puts "  #{seconds_in_an_hour * hours_in_a_day} seconds in a day,"
+puts "  #{seconds_in_an_hour * hours_in_a_day} seconds in an day,"
 puts "  #{seconds_in_an_hour * hours_in_a_day * days_in_a_week} seconds in a week"
 puts "That means when you turn #{my_age_now}, you've been alive for #{seconds_in_an_hour * hours_in_a_day * days_in_a_year * my_age_now} seconds,"
 puts "and if you make it to #{my_age_later}, you will have lived #{seconds_in_an_hour * hours_in_a_day * days_in_a_year * my_age_later} seconds. Make them count!"
