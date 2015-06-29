55842f3fe9349194b312764600bf5888e352c2f0
Sharon Sena <sharon.sena.g@gmail.com>
2015-06-28 18:38:26 -0600

    Added fizz_buzz.rb to methods and flow control
    [#96683962]

---
+++ methods_and_flow_control/fizz_buzz/fizz_buzz.rb
@@ -0,0 +1,16 @@
+#!usr#!usr/bin/env ruby
+num = 1
+
+while num <= 100
+  if num % 3 == 0 || num % 5 == 0
+    fizz = num % 3 == 0 ? "Fizz" :""
+    buzz = num % 5 == 0 ? "Buzz" :""
+  else
+    fizz = num.to_s
+    buzz = ""
+  end
+
+  puts fizz + buzz
+
+  num += 1
+  end
