b7a904fe53f575cdaac6ca1268a1295859bb00f4
Sharon Sena <sharon.sena.g@gmail.com>
2015-07-02 23:51:27 -0600

    Updated bottles_of_beer.rb
    [#96707180]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,7 +1,7 @@
 #!usr/bin/env ruby
 # 99 bottles of beer song
 def bottles(of_beer, ending)
-  of_beer == 2 ? ending_2 = "" : ending_2 = "s"
+  of_beer == 2 ? ending_2 = '' : ending_2 = 's'
   puts "#{of_beer} bottle#{ending} of beer on the wall, #{of_beer} bottle#{ending} of beer."
   puts "Take one down and pass it around, #{of_beer-1} bottle#{ending_2} of beer on the wall.\n\n"
 end
@@ -9,12 +9,12 @@ end
 def song
   99.downto(0) do |remaining|
     if remaining > 1
-      bottles(remaining, "s")
+      bottles(remaining, 's')
     elsif remaining == 1
-      bottles(remaining, "")
+      bottles(remaining, '')
     else
-      puts "No more bottles of beer on the wall, no more bottles of beer."
-      puts "Go to the store and buy some more, 99 bottles of beer on the wall."
+      puts 'No more bottles of beer on the wall, no more bottles of beer.'
+      puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
     end
   end
 end
