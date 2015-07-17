f1e6f97066409cc88c6e1fd16070a4b910c0e23f
Sharon Sena <sharon.sena.g@gmail.com>
2015-07-17 12:41:48 -0600

    Updated passed array_practice.rb
    [#96812228]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -9,32 +9,32 @@ number_2 = *9.downto(1)
 puts "T-10, #{number_2.join(", ")}...  BLASTOFF!"
 
 # "The last element is 10"
-number_3 = *1.upto(10)
+number_3 = *1.upto(10).to_a
 puts "The last element is #{number_3.last}"
 
 # "The first element is 1"
 puts "The first element is #{number_3.first}"
 
 # "The third element is 3"
-puts "The third element is #{number_3 [2]}"
+puts "The third element is #{number_3[2]}"
 
 # "The element with an index of 3 is 4"
-puts "The element with an index of #{number_3 [2]} is #{number_3 [3]}"
+puts "The element with an index of #{number_3[2]} is #{number_3[3]}"
 
 # "The second from last element is 9"
-puts "The second from the last element is #{number_3 [8]}"
+puts "The second from last element is #{number_3[8]}"
 
 # "The first four elements are '1, 2, 3, 4'"
 puts "The first four elements are '#{number_3.first(4).join(", ")}'"
 
 # "If we delete 5, 6 and 7 from the array, we're left with [1,2,3,4,8,9,10]"
-puts "If we delete 5, 6, and 7 from the array, we're left with #{number_3.take(4) + number_3.pop(3)}"
+puts "If we delete 5, 6 and 7 from the array, we're left with [#{number_3.take(4).join(",")},#{number_3.pop(3).join(",")}]"
 
 # "If we add 5 at the beginning of the array, we're left with [5,1,2,3,4,8,9,10]"
-puts "If we add 5 at the beginning of the array, we're left with #{number_3.unshift(5).take(5).push(8,9,10)}"
+puts "If we add 5 at the beginning of the array, we're left with [#{number_3.unshift(5).take(5).push(8,9,10).join(",")}]"
 
 # "If we add 6 at the end of the array, we're left with [5,1,2,3,4,8,9,10,6]"
-puts "If we add 6 at the end of the array, we're left with #{number_3.take(5).push(8,9,10,6)}"
+puts "If we add 6 at the end of the array, we're left with [#{number_3.take(5).push(8,9,10,6).join(",")}]"
 
 number_4 = *1.upto(10)
 # "Only the elements [9, 10] are > 8."
